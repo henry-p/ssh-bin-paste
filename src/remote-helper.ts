b5eb58e84726197a7b3eb35b1ee4df906ff7f409
Henry Perschk <henry.perschk@gmail.com>
2026-05-12 13:47:58 +0200

    remove node implementation

--- src/remote-helper.ts
+++
@@ -1,7 +0,0 @@
-import { AppConfig } from "./config.js";
-import { remotePathExpr, runSsh, shellQuote, CommandResult } from "./ssh.js";
-
-export async function runRemoteHelper(config: AppConfig, args: string[], input?: string | Buffer): Promise<CommandResult> {
-  const command = [remotePathExpr(config.remoteHelperPath), ...args.map(shellQuote)].join(" ");
-  return runSsh(config, command, input);
-}
